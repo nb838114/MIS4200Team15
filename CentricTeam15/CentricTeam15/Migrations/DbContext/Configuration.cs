b5625b9160668d2c4b513fa50c8900a874814a2d
Tori <vl163413@ohio.edu>
2017-11-01 11:20:16 -0400

    enabled data migrations

---
+++ CentricTeam15/CentricTeam15/Migrations/DbContext/Configuration.cs
@@ -0,0 +1,32 @@
+namespace CentricTeam15.Migrations.DbContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CentricTeam15.DAL.AccountDetailsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            MigrationsDirectory = @"Migrations\DbContext";
+        }
+
+        protected override void Seed(CentricTeam15.DAL.AccountDetailsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
